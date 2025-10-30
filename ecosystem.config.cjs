0d71ad36a4eb5d2a2f1fd3ae72d8150f4ca401c1
Zequiel <zequielwolf@gmail.com>
2025-10-30 21:55:17 +0100

    Update PM2 config to CommonJS format for ES module compatibility

---
+++ ecosystem.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [{
+    name: 'discord-selfbot',
+    script: 'bot.js',
+    env: {
+      // All environment variables should be set in .env file
+      // DO NOT hardcode secrets here
+    }
+  }]
+};
